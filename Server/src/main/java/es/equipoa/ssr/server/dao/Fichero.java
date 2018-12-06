878fdff9c7c4b89f0504f6339ec9434f74b11a52
Alvaro Crego <alvarocrego@users.noreply.github.com>
2018-12-06 15:54:23 +0100

    Nueva clase Fichero

---
+++ Server/src/main/java/es/equipoa/ssr/server/dao/Fichero.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.equipoa.ssr.server.dao;
+
+/**
+ *
+ * @author alcre
+ */
+public class Fichero {
+    
+    Integer id;
+    String nombre;
+    Integer idCliente;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(Integer idCliente) {
+        this.idCliente = idCliente;
+    }
+
+}
